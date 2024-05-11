7eecfea495597863a71ebd34a36f589b42531e85
Franciéllen Sousa Araújo <franciellensousaaraujo@gmail.com>
2024-05-10 21:23:12 -0300

    style: Add files styles

---
+++ lib/style/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+final themeDefault = ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+  seedColor: Color(0xF9F5F4),
+  onPrimary: Color(0xFF26185),
+  secondary: Color(0xFFEDAE),
+  onSecondary: Color(0x8C8E91),
+  onSurface: Colors.white,
+));
