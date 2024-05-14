caf05567dd0db6fb6ace03f2b3b04a0ff12ccbe7
Franciéllen Sousa Araújo <franciellensousaaraujo@gmail.com>
2024-05-14 18:06:07 -0300

    feat: Creates GridView to view the cards

---
+++ lib/screen/widget/grid_view.dart
@@ -0,0 +1,28 @@
+import 'package:app_plush_pan/models/categories.dart';
+import 'package:app_plush_pan/screen/widget/card.dart';
+import 'package:flutter/material.dart';
+
+class WidgetGridView extends StatelessWidget {
+  final List<Categories> categoriesDisplay;
+
+  const WidgetGridView({super.key, required this.categoriesDisplay});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          const EdgeInsets.only(left: 20.0, right: 20.0, top: 30, bottom: 10),
+      child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 12,
+          crossAxisSpacing: 0,
+        ),
+        itemCount: categoriesDisplay.length,
+        itemBuilder: (BuildContext context, int index) {
+          return WidgetCard(categories: categoriesDisplay[index]);
+        },
+      ),
+    );
+  }
+}
